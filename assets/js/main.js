ffbe9dcf656a24cbbd6fd216bf497dd487cf777d
bl00p1ng <blooping@protonmail.com>
2022-08-20 23:45:37 -0500

    Funcionalidad: mostrar/ocultar carrito de compras

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,16 @@ const navbarEmail = document.querySelector('.navbar-email')
 const navbarDesktopMenu = document.querySelector('.desktop-menu')
 const hamburguerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const navbarShoppingCart = document.querySelector('.navbar-shopping-cart')
+const productOrders = document.querySelector('.my-order')
 
 // EVENT LISTENERS
-navbarEmail.addEventListener('click', toggleDesktopMenu)
-
-hamburguerMenu.addEventListener('click', toggleMobileMenu)
+navbarEmail.addEventListener('click', () => toggleElement(navbarDesktopMenu, 'hidden'))
+hamburguerMenu.addEventListener('click', () => toggleElement(mobileMenu, 'slide-right'))
+navbarShoppingCart.addEventListener('click', () => toggleElement(productOrders, 'hidden'))
 
 // FUNCIONES
-// Mostrar u ocultar el menu del navbar en desktop
-function toggleDesktopMenu() {
-    navbarDesktopMenu.classList.toggle('hidden')
-}
-
-// Mostrar u ocultar el menu del mobile del navbar
-function toggleMobileMenu() {
-    mobileMenu.classList.toggle('slide-right')
+// Agregar o quitar una clase a un elemento
+const toggleElement = (element, className) => {
+    element.classList.toggle(className)
 }
