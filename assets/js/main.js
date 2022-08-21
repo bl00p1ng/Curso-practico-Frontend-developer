4b36878b1e3d23decc5dfc3500d682ca619a5888
bl00p1ng <blooping@protonmail.com>
2022-08-20 22:26:41 -0500

    Funcionalidad del navbar en mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 // SELECTORES
 const navbarEmail = document.querySelector('.navbar-email')
 const navbarDesktopMenu = document.querySelector('.desktop-menu')
-
-console.log(navbarEmail)
+const hamburguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 // EVENT LISTENERS
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 
+hamburguerMenu.addEventListener('click', toggleMobileMenu)
+
 // FUNCIONES
+// Mostrar u ocultar el menu del navbar en desktop
 function toggleDesktopMenu() {
     navbarDesktopMenu.classList.toggle('hidden')
+}
+
+// Mostrar u ocultar el menu del mobile del navbar
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('slide-right')
 }
